ffdec43c27e32ae13d74d9ac53b8b25eb10381f2
Stephen <stephent818@gmail.com>
2020-08-16 01:39:05 -0700

    floor and ceiling

--- src/entities/index.js
+++ src/entities/index.js
@@ -1,3 +1,5 @@
+import Ceiling from "../components/Ceiling";
+import Floor from "../components/Floor";
 import Matter from "matter-js";
 import Plane from "../components/Plane";
 
@@ -15,11 +17,23 @@ export default (restart) => {
 
   return {
     physics: { engine: engine, world: world },
+    Ceiling: Ceiling(
+      world,
+      "white",
+      { x: 1000 / 2, y: 0 },
+      { height: 100, width: 1000 }
+    ),
     Plane: Plane(
       world,
       "pink",
       { x: 220, y: 400 },
       { height: boxSize, width: boxSize }
+    ),
+    Floor: Floor(
+      world,
+      "white",
+      { x: 1000 / 2, y: 575 - 50 },
+      { height: 100, width: 1000 }
     ),
   };
 };
